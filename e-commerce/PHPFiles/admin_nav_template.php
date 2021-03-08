3a20a6423a67351382e55b4b4fae7fb5eaeebdb5
Andrew Scholly <SATmaster728@gmail.com>
2021-03-08 09:53:09 -0500

    completed shipments and members, fixed minor issues

---
+++ e-commerce/PHPFiles/admin_nav_template.php
@@ -0,0 +1,10 @@
+<<?php
+include login.php
+if ($row['role'] = 'admin') {
+  <ul>
+    <li><a href="e-commerce/adminpages/expenses.html">Expenses</li>
+    <li><a href="e-commerce/adminpages/management.html">Management</li>
+    <li><a href="e-commerce/adminpages/product.html">Products</li>
+    <li><a href="e-commerce/adminpages/shipments.html">shipments</li>
+  </ul>
+}
