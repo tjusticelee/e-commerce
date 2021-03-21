e8a0718e7969f8fe19f43c3ae4b383a48554f92f
Andrew Scholly <SATmaster728@gmail.com>
2021-03-21 13:26:07 -0400

    changed pages to php

--- e-commerce/PHPFiles/admin_nav_template.php
+++ e-commerce/PHPFiles/admin_nav_template.php
@@ -1,10 +1,11 @@
-<<?php
+<?php
 include login.php
 if ($row['role'] = 'admin') {
   <ul>
-    <li><a href="e-commerce/adminpages/expenses.html">Expenses</li>
-    <li><a href="e-commerce/adminpages/management.html">Management</li>
-    <li><a href="e-commerce/adminpages/product.html">Products</li>
-    <li><a href="e-commerce/adminpages/shipments.html">shipments</li>
+    <li><a href="e-commerce/adminpages/expenses.php">Expenses</li>
+    <li><a href="e-commerce/adminpages/management.php">Management</li>
+    <li><a href="e-commerce/adminpages/product.php">Products</li>
+    <li><a href="e-commerce/adminpages/shipments.php">shipments</li>
   </ul>
 }
+?>
